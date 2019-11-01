12a244d95d7eec462e134f36c90599cd7378b5ff
Mateusz Konczal <mateuszkonczal96@gmail.com>
2019-11-01 23:24:57 +0100

    Started project

---
+++ src/test/java/pl/edu/utp/wtie/homeworkweek6/HomeworkWeek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.wtie.homeworkweek6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkWeek6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
